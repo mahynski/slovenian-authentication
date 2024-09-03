68d1de31e67a591ba2bf34041e4024566e5f1764
Nathan A. Mahynski <nathan.mahynski@gmail.com>
2024-09-03 10:30:29 -0400

    updated devcontainers

--- .devcontainer/start_jupyter.sh
+++ .devcontainer/start_jupyter.sh
@@ -1,4 +1,4 @@
-# Start a Jupyter Notebook server with no password so we can easily access it
-source /opt/conda/etc/profile.d/conda.sh
-conda activate test-slo
-jupyter notebook --port 8501 --ip='*' --NotebookApp.token='' --NotebookApp.password=''
+#!/bin/bash
+source /opt/conda/etc/profile.d/conda.sh; # Start a Jupyter Notebook server with no password so we can easily access it
+conda activate test-slo;
+jupyter notebook --port 1234 --ip='*' --NotebookApp.token='' --NotebookApp.password='';
